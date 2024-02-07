8729f5de03c02062c684f172a26347f36758caa1
ak47k98 <2535210356@qq.com>
2024-02-07 21:53:40 +0800

    2024.02.07

--- OPEN CV/photo/hellob.py
+++ OPEN CV/photo/hellob.py
@@ -1,8 +1,7 @@
 import cv2 #opencv读取的格式是BGR
 import matplotlib.pyplot as plt
 import numpy as np 
-%matplotlib inline 
-img=cv2.imread('6-1.jpg')
+img = cv2.imread("6-1.jpg", cv2.IMREAD_GRAYSCALE)
 cv2.imshow("img",img)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
