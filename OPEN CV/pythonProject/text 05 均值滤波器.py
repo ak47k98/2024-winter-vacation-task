c8e58da3a4cb32465b3fd3914f13d87c0d21ed6a
ak47k98 <2535210356@qq.com>
2024-02-20 17:43:32 +0800

    20240220.04

--- OPEN CV/pythonProject/text 05 均值滤波器.py
+++ OPEN CV/pythonProject/text 05 均值滤波器.py
@@ -1,6 +1,8 @@
 import cv2
 image =cv2.imread("6-1.jpg")
 gauss = cv2.GaussianBlur(image,(5,5),0)#高斯滤波器 ，内核设置为五个像素
+median = cv2.medianBlur(image,5) #中值滤波器
 cv2.imshow("image",image)
 cv2.imshow("gauss",gauss)
+cv2.imshow("median",median)
 cv2.waitKey()
