0f80eb474dd4d012137b44e27351d4f36fbea13e
ak47k98 <2535210356@qq.com>
2024-02-21 20:06:32 +0800

    2024.02.21.updata01

--- OPEN CV/pythonProject/text 05 均值滤波器.py
+++ OPEN CV/pythonProject/text 05 均值滤波器.py
@@ -1,8 +1,22 @@
 import cv2
-image =cv2.imread("6-1.jpg")
-gauss = cv2.GaussianBlur(image,(5,5),0)#高斯滤波器 ，内核设置为五个像素
+image =cv2.imread("14-1.jpg")
+gauss = cv2.GaussianBlur(image,(7,7),0)#高斯滤波器 ，内核设置为五个像素   #奇数
 median = cv2.medianBlur(image,5) #中值滤波器
+imgCanny = cv2.Canny(image,150,200)
+"""
+cv2.Canny()函数是OpenCV中用于边缘检测的函数，其主要功能是检测图像中的边缘并标记出来。
+函数的调用格式如下：
+edges = cv2.Canny(image, threshold1, threshold2[, edges[, apertureSize[, L2gradient]]])
+其中，参数含义如下：
+    image：要进行边缘检测的输入图像，可以是灰度图像或彩色图像。
+    threshold1：第一个阈值，用于边缘检测中的滞后阈值，建议取值为100。
+    threshold2：第二个阈值，用于边缘检测中的滞后阈值，建议取值为200。
+    edges：可选参数，用于存储边缘检测的结果。如果指定了该参数，则该参数会被用来存储检测到的边缘图像。如果没有指定该参数，则函数会自动创建一个与输入图像相同大小的数组来存储检测到的边缘图像。
+    apertureSize：可选参数，用于指定Sobel算子的大小，建议取值为3。
+    L2gradient：可选参数，用于指定是否使用 L 2 L_2 L2​梯度计算方式。默认值为False，表示使用 L 1 L_1 L1​梯度计算方式。
+"""
 cv2.imshow("image",image)
 cv2.imshow("gauss",gauss)
 cv2.imshow("median",median)
+cv2.imshow("Canny",imgCanny)
 cv2.waitKey()
--- OPEN CV/pythonProject/text 06 图像特征提取.py
+++ OPEN CV/pythonProject/text 06 图像特征提取.py
@@ -1,9 +1,21 @@
 import cv2
-image = cv2.imread("配图2.jpg")
+image = cv2.imread("14-1.jpg")
 gray = cv2.cvtColor(image,cv2.COLOR_BGR2GRAY)#转化为灰度图
-corners = cv2.goodFeaturesToTrack(gray,500,0.1,10)#获取图像中的特征点,最多返回500个点，点的质量为0.1，点之间的距离不少于10个像素
+
+median = cv2.medianBlur(gray,5) #中值滤波器
+corners = cv2.goodFeaturesToTrack(median,500,0.1,10)#获取图像中的特征点,最多返回500个点，点的质量为0.1，点之间的距离不少于10个像素
+
 for corner in corners:
     x, y = corner.ravel()  #返回一个展平的数组  浮点数
     cv2.circle(image,(int(x),int(y)),3,(255,0,255),-1)
+
 cv2.imshow("corners",image)
+
+corners2 = cv2.goodFeaturesToTrack(gray,500,0.1,10)#获取图像中的特征点,最多返回500个点，点的质量为0.1，点之间的距离不少于10个像素
+
+for corner in corners2:
+    x, y = corner.ravel()  #返回一个展平的数组  浮点数
+    cv2.circle(image,(int(x),int(y)),3,(255,0,255),-1)
+
+cv2.imshow("corners2",image)
 cv2.waitKey()
