1dc21f259dce418259b38904ca758e83f2f79336
ak47k98 <2535210356@qq.com>
2024-03-23 17:43:24 +0800

    2024.03.23.02

--- OPEN CV/pythonProject/13.camera text/13.camera.py
+++ OPEN CV/pythonProject/13.camera text/13.camera.py
@@ -1,9 +1,11 @@
 import cv2
-#cap = cv2.VideoCapture(0)
 
-video="http://admin:admin@192.168.202.62:4747/video"      #更改ip
 
-cap =cv2.VideoCapture(video)
+cap = cv2.VideoCapture(0)
+
+#video="http://admin:admin@192.168.202.62:4747/video"      #更改ip
+
+#cap =cv2.VideoCapture(video)
 
 
 
@@ -18,7 +20,7 @@ while True :
     success,img = cap.read()
     #gauss = cv2.GaussianBlur(img, (7, 7), 0)  # 高斯滤波器 ，内核设置为五个像素   #奇数
     median = cv2.medianBlur(img, 5)  # 中值滤波器
-    imgCanny = cv2.Canny(img, 80, 200)
+    imgCanny = cv2.Canny(img, 80, 160)
     # 均值滤波
     img_blur = cv2.blur(img, (5, 5))
     FPS=cap.get(cv2.CAP_PROP_FPS)
--- OPEN CV/pythonProject/15.createTrackbar for face/15.py
+++ OPEN CV/pythonProject/15.createTrackbar for face/15.py
@@ -1,5 +1,4 @@
-import cv2
-import numpy as np
+
 """
 
 cv2.createTrackbar(“scale”, “display”, 0, 100, self.opencv_calibration_node.on_scale)
@@ -65,6 +64,10 @@ cv2.waitKey()
 
 
 """
+
+
+import cv2
+import numpy as np
 def nothing(x):
     pass
 
@@ -74,14 +77,19 @@ img = cv2.imread("14-1.jpg")
 # 创建窗口
 cv2.namedWindow("Blur Parameters")
 
+
+mean_val = cv2.mean(img)[0]
+#_, otsu_thresh = cv2.threshold(img, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)
+
 # 创建跟踪条，一个用于高斯模糊核大小，一个用于中值模糊核大小
+cv2.createTrackbar("threshold size","Blur Parameters",int(mean_val),255,nothing)
 cv2.createTrackbar("Gaussian Kernel Size", "Blur Parameters", 1, 30, nothing)
 cv2.createTrackbar("Median Kernel Size", "Blur Parameters", 1, 30, nothing)
-
 while(True):
     # 获取跟踪条的当前位置作为核大小
     gauss_size = cv2.getTrackbarPos("Gaussian Kernel Size", "Blur Parameters")
     median_size = cv2.getTrackbarPos("Median Kernel Size", "Blur Parameters")
+    threshold_size = cv2.getTrackbarPos("threshold size","Blur Parameters")
 
     # 确保核大小为正奇数
     gauss_size = max(1, gauss_size)
@@ -92,16 +100,25 @@ while(True):
         median_size += 1
 
     # 应用高斯滤波和中值滤波
-    gauss = cv2.GaussianBlur(img, (gauss_size, gauss_size), 0)
+    ret,image = cv2.threshold(img,threshold_size,255,cv2.THRESH_BINARY)
+    gauss = cv2.GaussianBlur(image, (gauss_size, gauss_size), 0)
     median = cv2.medianBlur(gauss, median_size)
+    gauss1 = cv2.GaussianBlur(img, (gauss_size, gauss_size), 0)
+    median1 = cv2.medianBlur(gauss1, median_size)
 
     # 在窗口中显示结果
+    cv2.imshow("threshold image",image)
     cv2.imshow("Gaussian Blurred", gauss)
     cv2.imshow("Median Blurred", median)
-
+    cv2.imshow("Gaussian Blurred1", gauss1)
+    cv2.imshow("Median Blurred1", median1)
     # 按 'q' 键退出循环
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
 
 # 关闭所有窗口
 cv2.destroyAllWindows()
+
+
+
+
